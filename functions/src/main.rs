d713228a3214f1571bd68256a5764ea6ee6ee6e6
eum602 <eum602@gmail.com>
2022-02-11 20:39:10 -0500

    functions

---
+++ functions/src/main.rs
@@ -0,0 +1,26 @@
+//silence warnings
+#![allow(unused_variables)]
+
+fn main() {
+    let width = 4;
+    let height = 7;
+    let depth = 10;
+
+    {
+        let area = area_of(width, height);
+        println!("Area is {}", area);
+    }
+
+    let calculated_volume = volume(width,height,depth);
+
+    println!("Volume is {}", calculated_volume)
+}
+
+fn area_of(x: i32, y: i32) -> i32 {
+    x*y
+}
+
+fn volume(x:i32,y:i32,z:i32)-> i32 {
+    x*y*z
+}
+
